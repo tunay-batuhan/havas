6469ae4e56c5f1e0dff6c51995741b69f29b1d06
Tunay Batuhan Çelik <50638690+tunay-batuhan@users.noreply.github.com>
2022-08-08 07:35:35 +0300

    havas case

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "../styles/components.scss";
 import "bootstrap/dist/css/bootstrap.css";
-import "react-calendar/dist/Calendar.css";
 import Layout from "../Layout/Layout";
 import { store } from "../store/store";
 import { Provider } from "react-redux";
